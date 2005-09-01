d2e65195068b74d4aa76c8128d9ea9ae2ebc88e5
umgangee <Unknown>
2005-09-01 19:48:29 +0000

    Modifications to reflect current state of PyBridge components.

--- pybridge/lib/core/scoring.py
+++ pybridge/lib/core/scoring.py
@@ -1,143 +1,121 @@
 from enumeration import Denomination, Seat
 
 
-class Scoring:
-	"""Base scoring class."""
-		
-	def __init__(self):
-		self.contractLevel = 0
-		self.trumpSuit = Denomination.Club
-		self.tricksMade = 0
-		self.declarer = Seat.North
-		self.doubled = False
-		self.redoubled = False
-		self.vulnerable = False
+# There are undoubtedly many minor variations of the score values.
+# In the future, score values may be stored in separate XML format files.
 
 
-class ScoreDuplicate(Scoring):
+def scoreDuplicate(self, result):
 	"""Scoring algorithm for duplicate bridge."""
+	score = 0
 
-	# There are undoubtedly many minor variations of the score values.
-	# In the future, score values may be stored in separate XML format files.
-	
-	def score(self):
-		theScore = 0
-		if self.tricksMade >= self.contractLevel + 6:
-			# Contract fulfilled.
-			
-			# Calculate scores for tricks bid and made.
-			if self.trumpSuit == 'c' or self.trumpSuit == 'd':
-				# Clubs and Diamonds score 20 for each odd trick.
-				theScore += self.contractLevel * 20
-			else:
-				# Hearts, Spades and NT score 30 for each odd trick.
-				theScore += self.contractLevel * 30
-				if self.trumpSuit == 'n':
-					# For NT, add a 10 point bonus.
-					theScore += 10
-			
-			# Calculate scores for doubles.
-			if self.doubled:
-				# Multiply score by 2 for each doubled odd trick.
-				theScore *= 2
-			elif self.redoubled:
-				# Multiply score by 4 for each redoubled odd trick.
-				theScore *= 4
-			
-			# Calculate premium scores.
-			if theScore >= 100:
-				if self.vulnerable:
-					# Game, vulnerable.
-					theScore += 500
-				else:
-					# Game, not vulnerable.
-					theScore += 300
-				if self.contractLevel == 7:
-					if self.vulnerable:
-						# Grand slam, vulnerable.
-						theScore += 1500
-					else:
-						# Grand slam, not vulnerable.
-						theScore += 1000
-				elif self.contractLevel == 6:
-					if self.vulnerable:
-						# Small slam, vulnerable.
-						theScore += 750
-					else:
-						# Small slam, not vulnerable.
-						theScore += 500
-				
-			else:
-				# Any part score.
-				theScore += 50
-			if self.doubled:
-				# Contract doubled, "for the insult".
-				theScore += 50
-			elif self.redoubled:
-				# Contract redoubled, "for the insult".
-				theScore += 100
-			
-			# Calculate scores for overtricks.
-			overTricks = self.tricksMade - self.contractLevel - 6
-			if overTricks > 0:
-				if self.doubled:
-					if self.vulnerable:
-						# Score 200 for each doubled and vulnerable overtrick.
-						theScore += overTricks * 200
-					else:
-						# Score 100 for each doubled and not vulnerable overtrick.
-						theScore += overTricks * 100
-				elif self.redoubled:
-					if self.vulnerable:
-						# Score 400 for each redoubled and vulnerable overtrick.
-						theScore += overTricks * 400
-					else:
-						theScore += overTricks * 200
-				else:
-					if self.trumpSuit == 'c' or self.trumpSuit == 'd':
-						# Clubs and Diamonds score 20 for each undoubled overtrick.
-						theScore += overTricks * 20
-					else:
-						# Hearts, Spades and NT score 30 for each undoubled overtrick.
-						theScore += overTricks * 30
-		
+	isDoubled      = result['contract']['doubleLevel'] == 1
+	isRedoubled    = result['contract']['doubleLevel'] == 2
+	isVulnerable   = result['vulnerable']
+	tricksMade     = result['tricksMade']
+	tricksRequired = result['contract']['bidLevel'] + 6
+	trumpSuit      = result['contract']['bidDenom']
+
+	if tricksMade >= tricksRequired:
+		# Contract fulfilled.
+
+		# Calculate scores for tricks bid and made.
+		if trumpSuit in (Denomination.Club, Denomination.Diamond):
+			# Clubs and Diamonds score 20 for each odd trick.
+			score += contractLevel * 20
 		else:
-			# Contract not fulfilled.
-			underTricks = self.contractLevel + 6 - self.tricksMade
-			if self.doubled:
-				if self.vulnerable:
-					# Score 200 for the first doubled and vulnerable undertrick
-					# and 300 for all other undertricks.
-					theScore -= 200 + (underTricks - 1) * 300
-				else:
-					# Score 100 for the first doubled and non-vulnerable
-					# undertrick and 300 for all other undertricks.
-					# Score 100 extra for undertricks.
+			# Hearts, Spades and NT score 30 for each odd trick.
+			score += contractLevel * 30
+			if trumpSuit is Denomination.NoTrump:
+				score += 10  # For NT, add a 10 point bonus.
 
-					theScore -= 100 + (underTricks - 1) * 200
-					if underTricks > 3:
-						theScore -= (underTricks - 3) * 100
-			elif self.redoubled:
-				if self.vulnerable:
-					theScore -= 400 + (underTricks - 1) * 600
-				else:
-					theScore -= 200 + (underTricks - 1) * 400
-					if underTricks > 3:
-						theScore -= (underTricks - 3) * 200
+		# Calculate scores for doubles.
+		if isDoubled:
+			score *= 2  # Multiply score by 2 for each isDoubled odd trick.
+		elif isRedoubled:
+			score *= 4  # Multiply score by 4 for each isRedoubled odd trick.
+
+		# Calculate premium scores.
+		if score >= 100:
+			if vulnerable:
+				score += 500  # Game, vulnerable.
 			else:
-				if self.vulnerable:
-					theScore -= 100 + (underTricks - 1) * 100
+				# Game, not vulnerable.
+				score += 300  # Game, not vulnerable.
+			if tricksRequired == 13:
+				if vulnerable:
+					score += 1500  # Grand slam, vulnerable.
 				else:
-					theScore -= 50 + (underTricks - 1) * 50
-				
-		return theScore
+					score += 1000  # Grand slam, not vulnerable.
+			elif contractLevel == 12:
+				if vulnerable:
+					score += 750  # Small slam, vulnerable.
+				else:
+					score += 500  # Small slam, not vulnerable.
+			
+		else:
+			score += 50  # Any part score.
+
+		# Calculate "for the insult" bonuses.
+		if isDoubled:
+			score += 50
+		elif isRedoubled:
+			score += 100
+		
+		# Calculate scores for overtricks.
+		overTricks = tricksMade - tricksRequired
+		if isDoubled:
+			if vulnerable:
+				# Score 200 for each doubled and vulnerable overtrick.
+				score += overTricks * 200
+			else:
+				# Score 100 for each doubled and not vulnerable overtrick.
+				score += overTricks * 100
+		elif isRedoubled:
+			if vulnerable:
+				# Score 400 for each redoubled and vulnerable overtrick.
+				score += overTricks * 400
+			else:
+				score += overTricks * 200
+		else:
+			if trumpSuit in (Denomination.Club, Denomination.Diamond):
+				# Clubs and Diamonds score 20 for each undoubled overtrick.
+				score += overTricks * 20
+			else:
+				# Hearts, Spades and NT score 30 for each undoubled overtrick.
+				score += overTricks * 30
+	
+	else:
+		# Contract not fulfilled.
+
+		underTricks = tricksRequired - tricksMade
+		if isDoubled:
+			if vulnerable:
+				# Score 200 for the first doubled and vulnerable undertrick.
+				# Score 300 for all other undertricks.
+				score -= 200 + (underTricks - 1) * 300
+			else:
+				# Score 100 for the first doubled and non-vulnerable undertrick.
+				# Score 200 for all other undertricks.
+				# Score 100 extra for third and greater undertricks.
+				score -= 100 + (underTricks - 1) * 200
+				if underTricks > 3:
+					score -= (underTricks - 3) * 100
+		elif isRedoubled:
+			if vulnerable:
+				score -= 400 + (underTricks - 1) * 600
+			else:
+				score -= 200 + (underTricks - 1) * 400
+				if underTricks > 3:
+					score -= (underTricks - 3) * 200
+		else:
+			if vulnerable:
+				score -= 100 + (underTricks - 1) * 100
+			else:
+				score -= 50 + (underTricks - 1) * 50
+
+	return score
 
 
-class ScoreRubber(Scoring):
-	"""Scoring algorithm for rubber bridge."""
-
-
-	# Not implemented.
-
-	def score(self):
-		return 0
+def scoreRubber(self, result):
+	pass  # TODO: implement.
