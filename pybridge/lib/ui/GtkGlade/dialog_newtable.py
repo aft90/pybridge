b3de29b45b3aa3d9c0ac6090ea3bf5d2f71b1118
umgangee <Unknown>
2005-09-23 20:15:30 +0000

    New table creation dialog. Subject to change.

---
+++ pybridge/lib/ui/GtkGlade/dialog_newtable.py
@@ -0,0 +1,40 @@
+# PyBridge -- online contract bridge made easy.
+# Copyright (C) 2004-2005 PyBridge Project.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+
+
+from wrapper import WindowWrapper
+
+
+class DialogNewtable(WindowWrapper):
+
+	window_name = 'dialog_newtable'
+
+
+	def new(self):
+		pass
+
+
+	# Signal handlers
+
+
+	def on_cancelbutton_clicked(self, widget, *args):
+		self.window.hide()
+
+	def on_okbutton_clicked(self, widget, *args):
+		title = self.title.get_text()
+		self.ui.connection.cmdTableCreate(title)
+		self.window.hide()
