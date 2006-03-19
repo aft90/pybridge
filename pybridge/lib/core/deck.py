1f29d9119176ccf5c0f3ba1c9e6857cbd3db9c0e
umgangee <Unknown>
2006-03-19 16:58:45 +0000

    Replaced "sanity checks" in code, with "Contracts for Python" contracts.

--- pybridge/lib/core/deck.py
+++ pybridge/lib/core/deck.py
@@ -1,24 +1,36 @@
 from enumeration import Rank, Suit, Seat
+import random  # To shuffle a deck.
 
 
 class Card:
+	"""A card has a rank and a suit."""
 
 
 	def __init__(self, rank, suit):
-		"""Initialises a card object with rank, suit and played flag."""
-		if rank in Rank.Ranks and suit in Suit.Suits:
-			self.rank, self.suit = rank, suit
-		else:
-			raise "Invalid specification of card."
+		"""
+		pre:
+			rank in Rank.Ranks
+			suit in Suit.Suits
+		"""
+		self.rank = rank
+		self.suit = suit
 
 
 	def __eq__(self, other):
-		"""Two cards are equivalent if they have the same rank and suit."""
+		"""Two cards are equivalent if they have the same rank and suit.
+		
+		pre:
+			isinstance(other, Card)
+		"""
 		return (self.suit == other.suit) and (self.rank == other.rank)
 
 
 	def __cmp__(self, other):
-		"""Compare cards for hand sorting."""
+		"""Compare cards for hand sorting.
+		
+		pre:
+			isinstance(other, Card)
+		"""
 		selfIndex = (Suit.Suits.index(self.suit) * 13) + Rank.Ranks.index(self.rank)
 		otherIndex = (Suit.Suits.index(other.suit) * 13) + Rank.Ranks.index(other.rank)
 		return cmp(selfIndex, otherIndex)
@@ -29,61 +41,61 @@ class Card:
 		return self.rank + " of " + self.suit + "s"
 
 
-class Hand(list):
-
-
-	def addCard(self, card):
-		"""Adds a specified card to the hand."""
-		if card not in self and len(self) < 13:
-			self.append(card)
-			return True
-		else:
-			return False
-
-
-	def removeCard(self, card):
-		"""Removes a specified card from the hand."""
-		if card in self:
-			self.remove(card)
-			return True
-		else:
-			return False
+# TODO: check that we really don't need this class, and remove it entirely.
+#class Hand(list):
+#
+#
+#	def addCard(self, card):
+#		"""Adds a specified card to the hand."""
+#		if card not in self and len(self) < 13:
+#			self.append(card)
+#			return True
+#		else:
+#			return False
+#
+#
+#	def removeCard(self, card):
+#		"""Removes a specified card from the hand."""
+#		if card in self:
+#			self.remove(card)
+#			return True
+#		else:
+#			return False
 
 
 class Deck:
+	"""A deck consists of 52 cards.
 
-
-	import random
-
+	inv:
+		len(self.cards) == 52
+	"""
 
 	def __init__(self):
-		"""Initialises a deck object with 52 card objects."""
-		self._cards = []
+		self.cards = []
 		for suit in Suit.Suits:
 			for rank in Rank.Ranks:
-				self._cards.append(Card(rank, suit))
+				self.cards.append(Card(rank, suit))
 
 
-	def _build(self):
-		"""Returns a dictionary of hands."""
-		return {Seat.North : Hand(), Seat.South : Hand(), Seat.East : Hand(), Seat.West : Hand()}
-
-
-	def generateOrdered(self, combination):
+	def dealOrdered(self, combination):
 		"""Returns a deal, determined by combination."""
 		pass  # NOT IMPLEMENTED YET.
 	
 
-	def generateRandom(self):
+	def dealRandom(self):
 		"""Returns a deal, from a shuffled deck."""
 		self.shuffle()
-		hands = self._build()
-		for index, card in enumerate(self._cards):
-			hands[Seat.Seats[index/13]].addCard(card)
+		hands = {
+			Seat.North : [],
+			Seat.South : [],
+			Seat.East  : [],
+			Seat.West  : [],
+		}
+		for index, card in enumerate(self.cards):
+			hands[Seat.Seats[index/13]].append(card)
 		return hands
 		
 
 	def shuffle(self):
 		"""Shuffles the cards."""
-		import random
-		random.shuffle(self._cards)
+		random.shuffle(self.cards)
