39cc2e537f4134f5346fec38f758e79105792b41
umgangee <Unknown>
2006-03-20 17:26:30 +0000

    Start of source tree restructuring.

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@
 
 from distutils.core import setup
 
-from pybridge.conf import *
-
-setup(  name=NAME,
-		version=VERSION,
-		description=COMMENTS,
-		url=URL,
-		author=AUTHORS,
-		author_email=AUTHORS_EMAIL,
-		packages=[NAME],
-		scripts=['scripts/' + NAME],
-	 )
+setup(name = 'pybridge',
+      version = '0.1',
+      author = 'Michael Banks',
+      author_email = 'michael@banksie.co.uk',
+      url = 'http://sourceforge.net/projects/pybridge',
+      description = 'Online bridge made easy.',
+      download_url = 'http://sourceforge.net/project/showfiles.php?group_id=114287',
+      packages = ['pybridge.client', 'pybridge.common', 'pybridge.server'],
+      scripts = ['scripts/pybridge-client.py', 'scripts/pybridge-server.py'],
+)
