5dd0ad0e8f8744d64534390a2925aa3f7ca32c32
umgangee <Unknown>
2006-03-21 12:50:37 +0000

    Some small edits.

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@
 
 from distutils.core import setup
 
-setup(name = 'pybridge',
-      version = '0.1',
-      author = 'Michael Banks',
-      author_email = 'michael@banksie.co.uk',
-      url = 'http://sourceforge.net/projects/pybridge',
-      description = 'Online bridge made easy.',
-      download_url = 'http://sourceforge.net/project/showfiles.php?group_id=114287',
-      packages = ['pybridge.client', 'pybridge.common', 'pybridge.server'],
-      scripts = ['scripts/pybridge-client.py', 'scripts/pybridge-server.py'],
+setup(
+	name = 'pybridge',
+	version = '0.1.0',
+	author = 'Michael Banks',
+	author_email = 'michael@banksie.co.uk',
+	url = 'http://sourceforge.net/projects/pybridge/',
+	description = 'Online bridge made easy.',
+	download_url = 'http://sourceforge.net/project/showfiles.php?group_id=114287',
+	packages = ['pybridge.client', 'pybridge.common', 'pybridge.server'],
+	scripts = ['scripts/pybridge-client.py', 'scripts/pybridge-server.py'],
+	package_data = {'pybridge.client' : ['pybridge.glade', 'images/*.png']},
 )
