c1d1fecf1ecd0182ab7f8c419c9481f216616472
umgangee <Unknown>
2006-03-27 12:26:28 +0000

    Manages windows. Sort of a replacement to the old UI class.

---
+++ pybridge/client/windowmanager.py
@@ -0,0 +1,72 @@
+# PyBridge -- online contract bridge made easy.
+# Copyright (C) 2004-2006 PyBridge Project.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+
+import gtk
+from twisted.internet import reactor
+
+from pybridge.environment import CLIENT_SETTINGS_PATH
+from settings import settings
+
+
+class WindowManager:
+
+
+	def __init__(self):
+		self.instances = {}
+
+
+	def launch(self, windowname):
+		"""Launches specified window."""
+		
+		if windowname == 'dialog_connection':
+			from dialog_connection import DialogConnection
+			instance = DialogConnection()
+		elif windowname == 'dialog_newtable':
+			from dialog_newtable import DialogNewtable
+			instance = DialogNewtable()
+		elif windowname == 'window_bidbox':
+			from window_bidbox import WindowBidbox
+			instance = WindowBidbox()
+		elif windowname == 'window_calls':
+			from window_calls import WindowCalls
+			instance = WindowCalls()
+		elif windowname == 'window_main':
+			from window_main import WindowMain
+			instance = WindowMain()
+		
+		self.instances[windowname] = instance
+
+
+	def destroy(self, windowname):
+		self.instances[windowname].window.destroy()
+		del self.instances[windowname]
+
+
+	def shutdown(self):
+		"""Bring everything to a stop, in a clean fashion."""
+		
+		# Save settings.
+		settings.write(file(CLIENT_SETTINGS_PATH, 'w'))
+		
+		# Close down the shop.
+		reactor.stop()
+		gtk.main_quit()
+
+
+windowmanager = WindowManager()
+
