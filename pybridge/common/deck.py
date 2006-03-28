d7eb130170cd9c2d0c61d2cdf14b9cdfd2db31d2
umgangee <Unknown>
2006-03-28 16:16:07 +0000

    Move Rank, Suit and Seat enumerations into this module. And a code cleanup.

--- pybridge/common/deck.py
+++ pybridge/common/deck.py
@@ -1,38 +1,60 @@
-from enumeration import Rank, Suit, Seat
+# PyBridge -- online contract bridge made easy.
+# Copyright (C) 2004-2006 PyBridge Project.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+
 import random  # To shuffle a deck.
+
+from pybridge.enum import Enum
+
+Rank = Enum('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine',
+             'Ten', 'Jack', 'Queen', 'King', 'Ace')
+
+Seat = Enum('North', 'East', 'South', 'West')  # Clockwise.
+
+Suit = Enum('Club', 'Diamond', 'Heart', 'Spade')
 
 
 class Card:
 	"""A card has a rank and a suit."""
 
-
 	def __init__(self, rank, suit):
-		"""
-		pre:
-			rank in Rank.Ranks
-			suit in Suit.Suits
-		"""
+		assert(rank in Rank)
+		assert(suit in Suit)
 		self.rank = rank
 		self.suit = suit
 
 
 	def __eq__(self, other):
-		"""Two cards are equivalent if they have the same rank and suit.
-		
-		pre:
-			isinstance(other, Card)
-		"""
-		return (self.suit == other.suit) and (self.rank == other.rank)
+		"""Two cards are equivalent if they have the same rank and suit."""
+		assert(isinstance(other, Card))
+		return self.suit == other.suit and self.rank == other.rank
 
 
 	def __cmp__(self, other):
 		"""Compare cards for hand sorting.
 		
+		Care must be taken when comparing cards of different suits.
+		
 		pre:
 			isinstance(other, Card)
 		"""
-		selfIndex = (Suit.Suits.index(self.suit) * 13) + Rank.Ranks.index(self.rank)
-		otherIndex = (Suit.Suits.index(other.suit) * 13) + Rank.Ranks.index(other.rank)
+		assert(isinstance(other, Card))
+		selfIndex = self.suit.index*13 + self.rank.index
+		otherIndex = other.suit.index*13 + other.rank.index
 		return cmp(selfIndex, otherIndex)
 
 
@@ -41,61 +63,33 @@ class Card:
 		return self.rank + " of " + self.suit + "s"
 
 
-# TODO: check that we really don't need this class, and remove it entirely.
-#class Hand(list):
-#
-#
-#	def addCard(self, card):
-#		"""Adds a specified card to the hand."""
-#		if card not in self and len(self) < 13:
-#			self.append(card)
-#			return True
-#		else:
-#			return False
-#
-#
-#	def removeCard(self, card):
-#		"""Removes a specified card from the hand."""
-#		if card in self:
-#			self.remove(card)
-#			return True
-#		else:
-#			return False
-
-
 class Deck:
 	"""A deck consists of 52 cards.
 
 	inv:
-		len(self.cards) == 52
+		len(self.cards) == len(Rank) * len(Suit)
 	"""
 
 	def __init__(self):
 		self.cards = []
-		for suit in Suit.Suits:
-			for rank in Rank.Ranks:
+		for suit in Suit:
+			for rank in Rank:
 				self.cards.append(Card(rank, suit))
 
 
 	def dealOrdered(self, combination):
-		"""Returns a deal, determined by combination."""
-		pass  # NOT IMPLEMENTED YET.
-	
+		"""Returns a deal, ordered by combination."""
+		pass  # Not implemented yet.
+
 
 	def dealRandom(self):
 		"""Returns a deal, from a shuffled deck."""
-		self.shuffle()
-		hands = {
-			Seat.North : [],
-			Seat.South : [],
-			Seat.East  : [],
-			Seat.West  : [],
-		}
-		for index, card in enumerate(self.cards):
-			hands[Seat.Seats[index/13]].append(card)
-		return hands
-		
-
-	def shuffle(self):
-		"""Shuffles the cards."""
 		random.shuffle(self.cards)
+		hands = {Seat.North : [],
+		         Seat.East  : [],
+		         Seat.South : [],
+		         Seat.West  : [], }
+		for index, card in enumerate(self.cards):
+			hands[Seat[index % len(Seat)]].append(card)
+		return hands
+
