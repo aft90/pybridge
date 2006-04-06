0e8bd605bb1f88afa1e14f9ba615d4b431cf89cc
umgangee <Unknown>
2006-04-06 19:02:38 +0000

    Updates to packaging script, so source dist works again.

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@
 
 from distutils.core import setup
 
+from pybridge.conf import PYBRIDGE_VERSION
+
 setup(
 	name = 'pybridge',
-	version = '0.1.0',
+	version = PYBRIDGE_VERSION,
 	author = 'Michael Banks',
 	author_email = 'michaelbanks@dsl.pipex.com',
 	url = 'http://sourceforge.net/projects/pybridge/',
@@ -12,5 +14,6 @@ setup(
 	download_url = 'http://sourceforge.net/project/showfiles.php?group_id=114287',
 	packages = ['pybridge', 'pybridge.client', 'pybridge.common', 'pybridge.server'],
 	scripts = ['bin/pybridge-client', 'bin/pybridge-server'],
-	package_data = {'pybridge.client' : ['pybridge.glade', 'images/*.png']},
+	package_data = {'pybridge.client' : ['glade/pybridge.glade', 'pixmaps/*.png']},
 )
+
