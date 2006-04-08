684e204c46cc2f43c7eb9786847f77eff7b7ef21
umgangee <Unknown>
2006-04-08 11:31:43 +0000

    Connector class now handles user events. It also abstracts remote calls, which is useful to UI code.

--- pybridge/client/connector.py
+++ pybridge/client/connector.py
@@ -20,17 +20,18 @@ from twisted.cred import credentials
 from twisted.internet import reactor
 from twisted.spread import pb
 
-from events import ClientEvents, TableEvents
+from table import ClientBridgeTable
+from windowmanager import windowmanager
 
 
-class Connector:
-	""""""
+class Connector(pb.Referenceable):
+	"""Provides the glue between the client code and the server."""
 
 
 	def __init__(self):
 		self.avatar = None
 		self.factory = pb.PBClientFactory()
-		self.tables = {}  # Remote table objects.
+		self.table = None  # ClientBridgeTable object.
 
 
 	def _connect(self, host, port, username, password):
@@ -42,19 +43,14 @@ class Connector:
 	
 		reactor.connectTCP(host, port, self.factory)
 		creds = credentials.UsernamePassword(username, password)
-		d = self.factory.login(creds, ClientEvents())
+		d = self.factory.login(creds, self)
 		d.addCallback(connected)
 		
 		return d
 
 
-	def _failure(self, failure):
-		"""For debugging purposes."""
-		print failure, failure.getErrorMessage()
-
-
 	def login(self, host, port, username, password):
-		""""""
+		"""Attempt to log in to server with username and password."""
 		d = self._connect(host, port, username, password)
 		return d
 
@@ -71,25 +67,73 @@ class Connector:
 		return d
 
 
-	def getTableEventHandler(self):
-		return TableEvents
+# Client request methods.
 
 
-	def callServer(self, requestName, **kwargs):  # request
-		"""Issues request to server."""
-		if self.avatar:
-			defer = self.avatar.callRemote(requestName, **kwargs)
-#			defer.addErrback(self._failure)
-			return defer
+	def hostTable(self, tablename):
+		
+		def success(table):
+			tableview.remote = table
+			self.table = tableview
+			self.table.setup()
+		
+		if not self.table:
+			tableview = ClientBridgeTable(tablename)
+			d = self.avatar.callRemote('hostTable', tablename=tablename, listener=tableview)
+			d.addCallback(success)
+			return d
 
 
-	def callTable(self, requestName, **kwargs):
-		"""Issues request to joined table."""
-		if len(self.tables) > 0:
-			table = self.tables.values()[0]
-			defer = table.callRemote(requestName, **kwargs)
-#			defer.addErrback(self._failure)
-			return defer
+	def joinTable(self, tablename):
+		
+		def success(table):
+			tableview.remote = table
+			self.table = tableview
+			return self.table.remote.callRemote('getState')
+		
+		if not self.table:
+			tableview = ClientBridgeTable(tablename)
+			d = self.avatar.callRemote('joinTable', tablename=tablename, listener=tableview)
+			d.addCallback(success)
+			d.addCallback(tableview.setup)  # To initialise the table variables.
+			return d
+
+
+	def leaveTable(self, tablename):
+		d = self.avatar.callRemote('leaveTable', tablename=tablename)
+		d.addCallback(lambda r: self.tables.pop(tablename))
+		return d
+
+
+	def listTables(self):
+		return self.avatar.callRemote('listTables')
+
+
+	def getTableInfo(self, tablename):
+		return self.avatar.callRemote('getTableInfo', tablename=tablename)
+
+
+# Event methods.
+
+
+	def remote_messageReceived(self, type, sender, message):
+		print "message " + message
+
+
+	def remote_tableOpened(self, tablename):
+		windowmanager.get('window_tablelisting').add_tables((tablename,))
+
+
+	def remote_tableClosed(self, tablename):
+		windowmanager.get('window_tablelisting').remove_tables((tablename,))
+
+
+	def remote_userLoggedIn(self, username):
+		print username + " connected"
+
+
+	def remote_userLoggedOut(self, username):
+		print username + " disconnected"
 
 
 connector = Connector()
