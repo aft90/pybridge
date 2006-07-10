1f687e99d02c2db935cf17d6b7016100f039ace5
umgangee <Unknown>
2006-07-10 15:59:51 +0000

    Changes to support frozen py2exe distributions of PyBridge.

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-
 from pybridge.conf import PYBRIDGE_VERSION
+
+# To create a standalone Windows executable using py2exe, run:
+# python setup.py py2exe
+
+try:
+	import py2exe
+except ImportError:
+	pass
+
+opts = {'py2exe': {'packages' : 'encodings',
+                   'includes' : 'cairo, pango, pangocairo, atk, gobject' } }
 
 setup(
 	name = 'pybridge',
@@ -10,10 +20,11 @@ setup(
 	author = 'Michael Banks',
 	author_email = 'michaelbanks@dsl.pipex.com',
 	url = 'http://sourceforge.net/projects/pybridge/',
-	description = 'Online bridge made easy.',
+	description = 'PyBridge allows you to play the card game of (contract) bridge, with human players, over the Internet or a local network.',
 	download_url = 'http://sourceforge.net/project/showfiles.php?group_id=114287',
 	packages = ['pybridge', 'pybridge.client', 'pybridge.common', 'pybridge.server'],
-	scripts = ['bin/pybridge-client', 'bin/pybridge-server'],
-	package_data = {'pybridge.client' : ['glade/pybridge.glade', 'pixmaps/*.png']},
+	scripts = ['bin/pybridge', 'bin/pybridge-server'],
+	package_data = {'pybridge' : ['glade/pybridge.glade', 'pixmaps/*.png']},
+	console = ['bin/pybridge', 'bin/pybridge-server'],
+	options = opts,
 )
-
