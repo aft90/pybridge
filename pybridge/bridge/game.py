5f7df896963be6a8374b700356d674475fa2d52f
umgangee <Unknown>
2006-07-31 20:07:26 +0000

    Changed methods with trick index parameters to use getTrick()-style tuples. Redefined "current trick" to include completed tricks (and not blank tricks).

--- pybridge/bridge/game.py
+++ pybridge/bridge/game.py
@@ -28,130 +28,169 @@ class GameError(Exception): pass
 
 
 class Game:
-	"""A bridge game models the bidding, playing, scoring sequence.
-	
-	"""
+    """A bridge game models the bidding, playing, scoring sequence.
+    
+    """
 
 
-	def __init__(self, dealer, deal, scoring, vulnNS, vulnEW):
-		"""Initialises game.
-		
-		scoring: instance of scoring class.
-		"""
-		self.vulnNS, self.vulnEW = vulnNS, vulnEW
-		self.contract = None
-		self.deal = deal
-		self.playing = None
-		self.scoring = scoring
-		
-		# Start bidding.
-		self.bidding = Bidding(dealer)
+    def __init__(self, dealer, deal, scoring, vulnNS, vulnEW):
+        """Initialises game.
+        
+        scoring: instance of scoring class.
+        """
+        self.vulnNS, self.vulnEW = vulnNS, vulnEW
+        self.deal = deal
+        self.playing = None
+        self.scoring = scoring
+        
+        # Start bidding.
+        self.bidding = Bidding(dealer)
 
 
-	def isComplete(self):
-		"""Returns True if game is complete, False otherwise."""
-		if self.playing:
-			return self.playing.isComplete()
-		else:
-			return self.bidding.isPassedOut()
+    def isComplete(self):
+        """Returns True if game is complete, False otherwise."""
+        if self.playing:
+            return self.playing.isComplete()
+        else:
+            return self.bidding.isPassedOut()
 
 
-	def isHandVisible(self, seat, viewer):
-		"""A hand is visible if one of the following conditions is met:
-		
-		1. The hand is the viewer's own hand.
-		2. Game is complete.
-		3. Bidding complete and viewer is dummy, who can see all hands.
-		4. Bidding complete and hand is dummy's, and first card of first trick played.
-		"""
-		return seat == viewer \
-		or self.isComplete() \
-		or (self.bidding.isComplete() and viewer == self.playing.dummy) \
-		or (self.bidding.isComplete() and seat == self.playing.dummy and len(self.playing.getTrick(0)[1]) >= 1)
+    def isHandVisible(self, seat, viewer):
+        """A hand is visible if one of the following conditions is met:
+        
+        1. The hand is the viewer's own hand.
+        2. Game is complete.
+        3. Bidding complete and viewer is dummy, who can see all hands.
+        4. Bidding complete and hand is dummy's, and first card of first trick played.
+        """
+        return seat == viewer \
+        or self.isComplete() \
+        or (self.bidding.isComplete() and viewer == self.playing.dummy) \
+        or (self.bidding.isComplete() and seat == self.playing.dummy and len(self.playing.getTrick(0)[1]) >= 1)
 
 
-	def makeCall(self, seat, call):
-		"""Makes call from seat."""
-		if self.bidding.isComplete():
-			raise GameError('not in bidding')
-		
-		if self.bidding.whoseTurn() is not seat:
-			raise GameError('out of turn')
-		elif not self.bidding.validCall(call):
-			raise GameError('invalid call')
-		
-		self.bidding.addCall(call)
-		
-		# If bidding is complete, start playing.
-		if self.bidding.isComplete() and not self.bidding.isPassedOut():
-			contract = self.bidding.contract()
-			trumpSuit = getattr(Suit, str(contract['bid'].strain), None)  # Convert.
-			self.playing = Playing(contract['declarer'], trumpSuit)
+    def makeCall(self, seat, call):
+        """Makes call from seat."""
+        if self.bidding.isComplete():
+            raise GameError('not in bidding')
+        
+        if self.bidding.whoseTurn() is not seat:
+            raise GameError('out of turn')
+        elif not self.bidding.validCall(call):
+            raise GameError('invalid call')
+        
+        self.bidding.addCall(call)
+        
+        # If bidding is complete, start playing.
+        if self.bidding.isComplete() and not self.bidding.isPassedOut():
+            contract = self.bidding.contract()
+            # Convert from bidding's Strain type to playing's Suit type.
+            # Note that No Trumps implies a None trump suit.
+            trumpSuit = getattr(Suit, str(contract['bid'].strain), None)
+            self.playing = Playing(contract['declarer'], trumpSuit)
 
 
-	def playCard(self, seat, card):
-		"""Plays card from seat."""
-		if not self.bidding.isComplete() or self.bidding.isPassedOut():
-			raise GameError('not in play')
-		elif self.playing.isComplete():
-			raise GameError('not in play')
-		
-		hand = self.deal[seat]
-		if self.playing.whoseTurn() is not seat:
-			raise GameError('out of turn')
-		elif not self.playing.isValidPlay(card, seat, hand):
-			raise GameError('invalid card')
-		
-		self.playing.playCard(card)
+    def playCard(self, seat, card):
+        """Plays card from seat."""
+        if not self.bidding.isComplete() or self.bidding.isPassedOut():
+            raise GameError('not in play')
+        elif self.playing.isComplete():
+            raise GameError('not in play')
+        
+        hand = self.deal[seat]
+        if self.playing.whoseTurn() is not seat:
+            raise GameError('out of turn')
+        elif not self.playing.isValidPlay(card, seat, hand):
+            raise GameError('invalid card')
+        
+        self.playing.playCard(card)
 
 
-	def getHand(self, seat, viewer=None):
-		"""Returns the hand of player specified by seat.
-		
-		If viewer player is specified, then the ability of viewer
-		to "see" the hand will be examined.
-		"""
-		if viewer and not self.isHandVisible(seat, viewer):
-			raise GameError('hand not visible')
-		return self.deal[seat]
+    def getHand(self, seat, viewer=None):
+        """Returns the hand of player specified by seat.
+        
+        If viewer player is specified, then the ability of viewer
+        to "see" the hand will be examined.
+        """
+        if viewer and not self.isHandVisible(seat, viewer):
+            raise GameError('hand not visible')
+        return self.deal[seat]
 
 
-	def score(self):
-		"""Returns the integer score value for declarer/dummy if:
+    def getTrickCount(self):
+        """
+        
+        @return['declarerWon']: number of tricks won by declarer/dummy.
+        @return['defendersWon']: number of tricks won by defenders.
+        @return['declarerNeeds']: number of extra tricks required by declarer
+                                  to make contract.
+        @return['declarerNeeds']: number of extra tricks required by defenders
+                                  to break contract.
+        @return['required']: number of tricks required from contract level.
+        
+        """
+        if self.playing is None:
+            raise GameError('not in play')
+        
+        count = dict.fromkeys(('declarerWon', 'declarerNeeds',
+                               'defendersWon', 'defendersNeed'), 0)
+        
+        for index in range(len(self.playing.winners)):
+            trick = self.playing.getTrick(index)
+            winner = self.playing.whoPlayed(self.playing.winningCard(trick))
+            if winner in (self.playing.declarer, self.playing.dummy):
+                count['declarerWon'] += 1
+            else:  # Trick won by defenders.
+                count ['defendersWon'] += 1
+        
+        contract = self.bidding.contract()
+        # Get index value of bid level, increment, add 6.
+        count['required'] = contract['bid'].level.index + 7
+        if count['required'] > count['declarerWon']:  # Declarer needs tricks.
+            count['declarerNeeds'] = count['required'] - count['declarerWon']
+        if count['required'] > count['defendersWon']:  # Defenders need tricks.
+            count['defendersNeed'] = 13 - count['required'] - count['defendersWon'] + 1
+        
+        return count
 
-		- bidding stage has been passed out, with no bids made.
-		- play stage is complete.
-		"""
-		if not self.isComplete():
-			raise GameError('game not complete')
-		elif self.bidding.isPassedOut():
-			return 0  # A passed out deal does not score.
-		else:
-			contract = self.bidding.contract()
-			declarer = contract['declarer']
-			dummy = Seat[(declarer.index + 2) % 4]
-			vulnerable = (self.vulnNS and declarer in (Seat.North, Seat.South)) + \
-			             (self.vulnEW and declarer in (Seat.West, Seat.East))
-			
-			tricksMade = 0  # Count of tricks won by declarer or dummy.
-			for trickindex in range(len(self.playing.winners)):
-				winCard = self.playing.winningCard(trickindex)
-				winPlayer = self.playing.whoPlayed(winCard)
-				tricksMade += winPlayer in (declarer, dummy)
-			
-			result = {'contract'   : self.bidding.contract(),
-			          'tricksMade' : tricksMade,
-			          'vulnerable' : vulnerable, }
-			return self.scoring(result)
+
+    def score(self):
+        """Returns the integer score value for declarer/dummy if:
+
+        - bidding stage has been passed out, with no bids made.
+        - play stage is complete.
+        """
+        if not self.isComplete():
+            raise GameError('game not complete')
+        elif self.bidding.isPassedOut():
+            return 0  # A passed out deal does not score.
+        else:
+            contract = self.bidding.contract()
+            declarer = contract['declarer']
+            dummy = Seat[(declarer.index + 2) % 4]
+            vulnerable = (self.vulnNS and declarer in (Seat.North, Seat.South)) + \
+                         (self.vulnEW and declarer in (Seat.West, Seat.East))
+            
+            tricksMade = 0  # Count of tricks won by declarer or dummy.
+            for index in range(len(self.playing.winners)):
+                trick = self.playing.getTrick(index)
+                winningCard = self.playing.winningCard(trick)
+                winner = self.playing.whoPlayed(winningCard)
+                tricksMade += winner in (declarer, dummy)
+            
+            result = {'contract'   : self.bidding.contract(),
+                      'tricksMade' : tricksMade,
+                      'vulnerable' : vulnerable, }
+            return self.scoring(result)
 
 
-	def whoseTurn(self):
-		"""Returns the seat that is next to call or play card."""
-		if not self.isComplete():
-			if self.bidding.isComplete():
-				return self.playing.whoseTurn()
-			else:
-				return self.bidding.whoseTurn()
-		else:
-			raise GameError('game complete')
+    def whoseTurn(self):
+        """Returns the seat that is next to call or play card."""
+        if not self.isComplete():
+            if self.bidding.isComplete():
+                return self.playing.whoseTurn()
+            else:
+                return self.bidding.whoseTurn()
+        else:
+            raise GameError('game complete')
 
--- pybridge/bridge/playing.py
+++ pybridge/bridge/playing.py
@@ -24,137 +24,146 @@ from deck import Seat
 
 
 class Playing:
-	"""This class represents the playing of cards into tricks.
+    """This class represents the playing of cards into tricks.
 
-	This code is generalised, and could easily be adapted to support a
-	variety of trick-taking card games.
-	"""
+    This code is generalised, and could easily be adapted to support a
+    variety of trick-taking card games.
+    """
 
-	def __init__(self, declarer, trumps):
-		assert declarer in Seat
-		assert trumps in Suit or trumps is None  # (None = No Trumps)
-		self.trumps = trumps
-		
-		self.declarer = declarer
-		self.dummy = Seat[(declarer.index + 2) % 4]
-		self.lho = Seat[(declarer.index + 1) % 4]
-		self.rho = Seat[(declarer.index + 3) % 4]
-		
-		# Each trick corresponds to a cross-section of lists.
-		self.played = {Seat.North : [], Seat.East : [],
-		               Seat.South : [], Seat.West : [], }
-		# Winning player of each trick.
-		self.winners = []
-
-
-	def isComplete(self):
-		"""Returns true for 13 complete tricks."""
-		return len(self.winners) == 13
+    def __init__(self, declarer, trumps):
+        assert declarer in Seat
+        assert trumps in Suit or trumps is None  # (None = No Trumps)
+        self.trumps = trumps
+        
+        self.declarer = declarer
+        self.dummy = Seat[(declarer.index + 2) % 4]
+        self.lho = Seat[(declarer.index + 1) % 4]
+        self.rho = Seat[(declarer.index + 3) % 4]
+        
+        # Each trick corresponds to a cross-section of lists.
+        self.played = {}
+        for seat in Seat:
+            self.played[seat] = []
+        self.winners = []  # Winning player of each trick.
 
 
-	def currentTrick(self):
-		"""Returns index of current trick."""
-		return not self.isComplete() and len(self.winners)
+    def isComplete(self):
+        """Returns true for 13 complete tricks."""
+        return len(self.winners) == 13
 
 
-	def getTrick(self, trickindex):
-		"""Returns a tuple specifying:
-		
-		- the leader seat.
-		- a dict containing seat : card pairs, for each card in trick.
-		"""
-		if trickindex == 0:  # First trick.
-			leader = self.lho  # Leader is declarer's left-hand opponent.
-		else:  # Leader is winner of previous trick.
-			leader = self.winners[trickindex - 1]
-		trick = {}
-		for seat in Seat:
-			if len(self.played[seat]) > trickindex:
-				trick[seat] = self.played[seat][trickindex]
-		return leader, trick
+    def getTrick(self, index):
+        """Returns a tuple specifying:
+        
+        - the leader seat.
+        - a dict containing seat : card pairs, for each card in trick.
+        """
+        if index == 0:  # First trick.
+            leader = self.lho  # Leader is declarer's left-hand opponent.
+        else:  # Leader is winner of previous trick.
+            leader = self.winners[index - 1]
+        cards = {}
+        for seat in Seat:
+            # If length of list exceeds index value, player's card in trick.
+            if len(self.played[seat]) > index:
+                cards[seat] = self.played[seat][index]
+        return leader, cards
 
 
-	def playCard(self, card):
-		"""Plays card to current trick. Assumes correct seat.
-		
-		Card validity should be checked with isValidPlay() beforehand.
-		"""
-		assert isinstance(card, Card)
-		# Skip the seat and hand checks here.
-		assert self.isValidPlay(card, self.whoseTurn(), [card])
-		
-		self.played[self.whoseTurn()].append(card)
-		
-		# If trick is complete, determine winner.
-		if len(self.getTrick(self.currentTrick())[1]) == 4:
-			winner = self.whoPlayed(self.winningCard(self.currentTrick()))
-			self.winners.append(winner)
-		
-
-	def isValidPlay(self, card, seat=None, hand=[]):
-		"""Card is playable if and only if:
-		
-		- Play session is not complete.
-		- Seat is on turn to play.
-		- Card exists in hand.
-		- Card has not been previously played.
-		
-		In addition, if the current trick has an established lead, then
-		card must follow lead suit OR hand must be void in lead suit.
-		
-		Specification of seat and hand  required for verification.
-		"""
-		if self.isComplete():
-			return False
-		elif hand and card not in hand:
-			return False  # Playing a card not in hand.
-		elif seat and seat is not self.whoseTurn():
-			return False  # Playing out of turn.
-		elif self.whoPlayed(card):
-			return False  # Card played previously.
-		
-		leader, trick = self.getTrick(self.currentTrick())
-		if len(trick) == 0:
-			return True  # Card is the first card in trick.
-		
-		else:  # Current trick has an established lead: check for revoke.
-			leadcard = trick[leader]
-			# Cards in hand that match suit of leadcard.
-			followers = [c for c in hand if c.suit == leadcard.suit and not self.whoPlayed(c)]
-			# Hand void in lead suit or card follows lead suit.
-			return len(followers) == 0 or card in followers
+    def getCurrentTrick(self):
+        """Returns the getTrick() tuple of the current trick."""
+        i = max([len(cards) for cards in self.played.values()])
+        index = (i >= 1 and i-1) or 0
+        return self.getTrick(index)
 
 
-	def whoPlayed(self, card):
-		"""If card is played, returns seat of player, otherwise False."""
-		for seat, cards in self.played.items():
-			if card in cards:
-				return seat
-		return False
+    def playCard(self, card):
+        """Plays card to current trick. Assumes correct seat.
+        
+        Card validity should be checked with isValidPlay() beforehand.
+        """
+        assert isinstance(card, Card)
+        # Skip the seat and hand checks here.
+        assert self.isValidPlay(card, self.whoseTurn(), [card])
+        
+        self.played[self.whoseTurn()].append(card)
+        
+        # If trick is complete, determine winner.
+        leader, cards = self.getCurrentTrick()
+        if len(cards) == 4:
+            winner = self.whoPlayed(self.winningCard(self.getCurrentTrick()))
+            self.winners.append(winner)
 
 
-	def whoseTurn(self):
-		"""Returns the seat that is on turn to play."""
-		if not self.isComplete():
-			leader, trick = self.getTrick(self.currentTrick())
-			return Seat[(leader.index + len(trick)) % 4]
-		return False
+    def isValidPlay(self, card, seat=None, hand=[]):
+        """Card is playable if and only if:
+        
+        - Play session is not complete.
+        - Seat is on turn to play.
+        - Card exists in hand.
+        - Card has not been previously played.
+        
+        In addition, if the current trick has an established lead, then
+        card must follow lead suit OR hand must be void in lead suit.
+        
+        Specification of seat and hand  required for verification.
+        """
+        if self.isComplete():
+            return False
+        elif hand and card not in hand:
+            return False  # Playing a card not in hand.
+        elif seat and seat is not self.whoseTurn():
+            return False  # Playing out of turn.
+        elif self.whoPlayed(card):
+            return False  # Card played previously.
+        
+        leader, cards = self.getCurrentTrick()
+        # 0 if start of playing, 4 if complete trick.
+        if len(cards) in (0, 4):
+            return True  # Card will be first in next trick.
+        
+        else:  # Current trick has an established lead: check for revoke.
+            leadcard = cards[leader]
+            # Cards in hand that match suit of leadcard.
+            followers = [c for c in hand if c.suit == leadcard.suit and not self.whoPlayed(c)]
+            # Hand void in lead suit or card follows lead suit.
+            return len(followers) == 0 or card in followers
 
 
-	def winningCard(self, trickindex):
-		"""Determine which card wins the specified trick:
-		
-		- In a trump contract, the highest ranked trump card wins.
-		- Otherwise, the highest ranked card of the lead suit wins.
-		"""
-		leader, trick = self.getTrick(trickindex)
-		if len(trick) == 4:  # Trick is complete.
-			if self.trumps:  # Suit contract.
-				trumpcards = [c for c in trick.values() if c.suit==self.trumps]
-				if len(trumpcards) > 0:
-					return max(trumpcards)  # Highest ranked trump.
-			# No Trump contract, or no trump cards played.
-			followers = [c for c in trick.values() if c.suit==trick[leader].suit]
-			return max(followers)  # Highest ranked card in lead suit.
-		return False
+    def whoPlayed(self, card):
+        """If card is played, returns seat of player, otherwise False."""
+        for seat, cards in self.played.items():
+            if card in cards:
+                return seat
+        return False
+
+
+    def whoseTurn(self):
+        """Returns the seat that is on turn to play."""
+        if not self.isComplete():
+            trick = self.getCurrentTrick()
+            leader, cards = trick
+            if len(cards) == 4:  # If trick is complete, the trick winner's turn.
+                return self.whoPlayed(self.winningCard(trick))
+            else:
+                return Seat[(leader.index + len(cards)) % 4]
+        return False
+
+
+    def winningCard(self, trick):
+        """Determine which card wins the specified trick:
+        
+        - In a trump contract, the highest ranked trump card wins.
+        - Otherwise, the highest ranked card of the lead suit wins.
+        """
+        leader, cards = trick
+        if len(cards) == 4:  # Trick is complete.
+            if self.trumps:  # Suit contract.
+                trumpcards = [c for c in cards.values() if c.suit==self.trumps]
+                if len(trumpcards) > 0:
+                    return max(trumpcards)  # Highest ranked trump.
+            # No Trump contract, or no trump cards played.
+            followers = [c for c in cards.values() if c.suit==cards[leader].suit]
+            return max(followers)  # Highest ranked card in lead suit.
+        return False
 
