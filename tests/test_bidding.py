ba5360abe8156b8537a88ddd9bba03d1059a8a3b
umgangee <Unknown>
2006-08-11 13:28:46 +0000

    Some test cases for the Bidding and Playing classes.

---
+++ tests/test_bidding.py
@@ -0,0 +1,73 @@
+import unittest
+import random
+
+from pybridge.bridge.bidding import Bidding
+from pybridge.bridge.call import Bid, Pass, Double, Redouble
+
+# Enumerations.
+from pybridge.bridge.call import Level, Strain
+from pybridge.bridge.deck import Seat
+
+
+class TestBidding(unittest.TestCase):
+
+
+    # Generate some bids.
+    bids = [Bid(l, s) for l in Level for s in Strain]
+
+
+
+    def setUp(self):
+        dealer = random.choice(Seat)
+        self.bidding = Bidding(dealer)
+
+
+    def tearDown(self):
+        self.bidding = None
+
+
+    def testGetCurrentCall(self):
+        """getCurrentCall"""
+        for calltype in [Bid, Pass, Double, Redouble]:
+            self.assertEqual(self.bidding.getCurrentCall(calltype), None)
+        
+        for call, calltype in [(Pass(), Pass), (Bid(Level.One, Strain.Club), Bid),
+                               (Double(), Double), (Redouble(), Redouble)]:
+            self.assertEqual(self.bidding.getCurrentCall(calltype), None)
+            self.bidding.makeCall(call)
+            self.assertEqual(self.bidding.getCurrentCall(calltype), call)
+        
+        # A bid should clear types Pass, Double, Redouble.
+        bid = Bid(Level.One, Strain.Diamond)
+        self.bidding.makeCall(bid)
+        self.assertEqual(self.bidding.getCurrentCall(Bid), bid)
+        for calltype in [Pass, Double, Redouble]:
+            self.assertEqual(self.bidding.getCurrentCall(calltype), None)
+        
+
+    def testWhoseTurn(self):
+        """whoseTurn"""
+        # Tests whoseTurn() before and after making calls.
+        turn = Seat[self.bidding.dealer.index]
+        for call in self.bids:
+            self.assertEqual(self.bidding.whoseTurn(), turn)
+            self.bidding.makeCall(call)
+            turn = Seat[(turn.index + 1) % 4]
+            self.assertEqual(self.bidding.whoseTurn(), turn)
+
+
+    def testIsValidCall(self):
+        """isValidCall"""
+        pass
+
+
+
+
+def main():
+    suite = unittest.makeSuite(TestBidding)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ tests/test_playing.py
@@ -0,0 +1,59 @@
+import unittest
+import random
+
+from pybridge.bridge.card import Card
+from pybridge.bridge.deck import Deck
+from pybridge.bridge.playing import Playing
+
+# Enumerations.
+from pybridge.bridge.card import Suit
+from pybridge.bridge.deck import Seat
+
+
+class TestPlaying(unittest.TestCase):
+
+    
+    def setUp(self):
+        declarer = random.choice(Seat)
+        trumps = random.choice(list(Suit) + [None])
+        self.deal = Deck().dealRandom()
+        self.playing = Playing(declarer, trumps)
+
+
+    def tearDown(self):
+        self.deal = None
+        self.playing = None
+
+
+    def testWhoseTurn(self):
+        """whoseTurn"""
+        while not self.playing.isComplete():
+            # Determine whose turn it should be.
+            trick = self.playing.getCurrentTrick()
+            leader, cards = trick
+            if len(cards) == 4:
+                turn = self.playing.whoPlayed(self.playing.winningCard(trick))
+            else:
+                turn = Seat[(leader.index + len(cards)) % 4]
+
+            # Check that whoseTurn() works.
+            self.assertEqual(self.playing.whoseTurn(), turn)
+            
+            # Play a card for the next iteration.
+            hand = self.deal[turn]
+            for card in hand:
+                if self.playing.isValidPlay(card, turn, hand):
+                    break
+            self.playing.playCard(card, turn, hand)
+
+
+
+
+def main():
+    suite = unittest.makeSuite(TestPlaying)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+if __name__ == '__main__':
+    main()
+
