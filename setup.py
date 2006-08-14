1e6436c3a8e0b5d98a06628c555d3aa2c234a440
umgangee <Unknown>
2006-08-14 12:17:41 +0000

    Updates for packaging version 0.2.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 import glob
 from distutils.core import setup
-from pybridge.conf import PYBRIDGE_VERSION
+from pybridge import __version__
 
 # To create a standalone Windows executable using py2exe, run:
 # python setup.py py2exe
@@ -17,17 +17,20 @@ opts = {'py2exe': {'packages' : 'encodings',
 
 setup(
 	name = 'pybridge',
-	version = PYBRIDGE_VERSION,
+	version = __version__,
 	author = 'Michael Banks',
-	author_email = 'michaelbanks@dsl.pipex.com',
+	author_email = 'michael@banksie.co.uk',
 	url = 'http://sourceforge.net/projects/pybridge/',
 	description = 'A free online bridge game.',
 	download_url = 'http://sourceforge.net/project/showfiles.php?group_id=114287',
-	packages = ['pybridge', 'pybridge.client', 'pybridge.common', 'pybridge.server'],
+	packages = ['pybridge', 'pybridge.bridge', 'pybridge.interfaces',
+                'pybridge.network', 'pybridge.server', 'pybridge.ui'],
 	scripts = ['bin/pybridge', 'bin/pybridge-server'],
-	package_data = {'pybridge' : ['glade/pybridge.glade', 'pixmaps/*.png']},
+	package_data = {'pybridge' : ['glade/pybridge.glade', 'locale/' 'pixmaps/*.png']},
 	console = ['bin/pybridge', 'bin/pybridge-server'],
-	data_files = [('glade', glob.glob('glade/*.glade')), ('pixmaps', glob.glob('pixmaps/*.png')) ],
+	data_files = [('glade', glob.glob('glade/*.glade')),
+                  ('locale', glob.glob('locale/')),
+                  ('pixmaps', glob.glob('pixmaps/*.png')), ],
 
 	options = opts,
 )
