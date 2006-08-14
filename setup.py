9b64d8fe9c4424d4f495dda05b73608d5093c968
umgangee <Unknown>
2006-08-14 14:22:48 +0000

    Updated distutils setup script for version 0.2.0.

--- setup.py
+++ setup.py
@@ -4,33 +4,34 @@ import glob
 from distutils.core import setup
 from pybridge import __version__
 
-# To create a standalone Windows executable using py2exe, run:
-# python setup.py py2exe
+# To create a standalone Windows distribution, run "python setup.py py2exe"
+# and then copy the GTK/etc and GTK/lib directories into dist/
+
 
 try:
-	import py2exe
+    import py2exe
 except ImportError:
-	pass
-
-opts = {'py2exe': {'packages' : 'encodings',
-                   'includes' : 'pango, atk, gobject, gtk, gtk.glade' } }
+    pass
 
 setup(
-	name = 'pybridge',
-	version = __version__,
-	author = 'Michael Banks',
-	author_email = 'michael@banksie.co.uk',
-	url = 'http://sourceforge.net/projects/pybridge/',
-	description = 'A free online bridge game.',
-	download_url = 'http://sourceforge.net/project/showfiles.php?group_id=114287',
-	packages = ['pybridge', 'pybridge.bridge', 'pybridge.interfaces',
-                'pybridge.network', 'pybridge.server', 'pybridge.ui'],
-	scripts = ['bin/pybridge', 'bin/pybridge-server'],
-	package_data = {'pybridge' : ['glade/pybridge.glade', 'locale/' 'pixmaps/*.png']},
-	console = ['bin/pybridge', 'bin/pybridge-server'],
-	data_files = [('glade', glob.glob('glade/*.glade')),
-                  ('locale', glob.glob('locale/')),
-                  ('pixmaps', glob.glob('pixmaps/*.png')), ],
-
-	options = opts,
+    name = 'pybridge',
+    version = __version__,
+    author = 'Michael Banks',
+    author_email = 'michael@banksie.co.uk',
+    url = 'http://sourceforge.net/projects/pybridge/',
+    description = 'A free online bridge game.',
+    download_url = 'http://sourceforge.net/project/showfiles.php?group_id=114287',
+    packages = ['pybridge', 'pybridge.bridge', 'pybridge.interfaces', 'pybridge.network', 'pybridge.server', 'pybridge.ui'],
+    package_data = {'pybridge' : ['glade/pybridge.glade', 'locale', 'pixmaps/*']},
+    scripts = ['bin/pybridge', 'bin/pybridge-server'],
+    
+    # py2exe
+    console = ['bin/pybridge-server'],
+    windows = [{'script' : 'bin/pybridge', 'icon_resources' : [(1, 'pixmaps/pybridge.ico')]}],
+    data_files = [('.', ['AUTHORS', 'COPYING', 'INSTALL', 'NEWS', 'README']),
+	          ('glade', glob.glob('glade/*.glade')),
+                  ('pixmaps', glob.glob('pixmaps/*')), ],
+    options = {'py2exe': {'packages' : 'encodings',
+                          'includes' : 'cairo, pango, pangocairo, atk, gobject, gtk.glade' } },
 )
+
