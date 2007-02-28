b04bdc3a03d5ffa982524d42de1b8ced6d27ec79
umgangee <Unknown>
2007-02-28 18:11:30 +0000

    Expanded docstrings of error classes.

--- pybridge/network/error.py
+++ pybridge/network/error.py
@@ -1,5 +1,5 @@
 # PyBridge -- online contract bridge made easy.
-# Copyright (C) 2004-2006 PyBridge Project.
+# Copyright (C) 2004-2007 PyBridge Project.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -10,7 +10,7 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
@@ -19,11 +19,17 @@
 from twisted.spread import pb
 
 
-# Base errors.
-
 class DeniedRequest(pb.Error):
-    """Raised in response to an unsatisfiable request."""
+    """Raised by server in response to an unsatisfiable request from client."""
+
 
 class IllegalRequest(pb.Error):
-    """Raised in response to an erroneous request."""
+    """Raised by server in response to an erroneous request from client.
+    
+    The propagation of this error from server to client suggests there is a bug
+    in the client code (ie. sending invalid or ill-formatted data to the server)
+    or in the server code (ie. mishandling data).
+    
+    Please report any bugs which you discover in PyBridge!
+    """
 
