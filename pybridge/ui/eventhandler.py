485e5426192e84d798e1e4c3a29fb76b79364609
umgangee <Unknown>
2007-04-01 17:41:14 +0000

    SimpleEventHandler implementation of IListener: redirects update() calls to methods of its target object.

--- pybridge/ui/eventhandler.py
+++ pybridge/ui/eventhandler.py
@@ -1,5 +1,5 @@
 # PyBridge -- online contract bridge made easy.
-# Copyright (C) 2004-2006 PyBridge Project.
+# Copyright (C) 2004-2007 PyBridge Project.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -10,7 +10,7 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
@@ -18,15 +18,33 @@
 
 from zope.interface import implements
 
-from pybridge.interfaces.table import ITableEvents
-from pybridge.interfaces.bridgetable import IBridgeTableEvents
-from pybridge.interfaces.serverstate import IServerEvents
+from pybridge.interfaces.observer import IListener
+
+
+class SimpleEventHandler:
+    """An implementation of IListener which redirects updates to its target."""
+
+    implements(IListener)
+
+
+    def __init__(self, target, prefix='event_'):
+        self.__target = target
+        self.__prefix = prefix
+
+
+    def update(self, event, *args, **kwargs):
+        """Redirects named event to target's handler method, if present."""
+        method = getattr(self.__target, "%s%s" % (self.__prefix, event), None)
+        if method:
+            method(*args, **kwargs)
+
+
 
 
 class EventHandler:
     """An implementation of ITableEvents."""
 
-    implements(IServerEvents, ITableEvents, IBridgeTableEvents)
+    #implements(IServerEvents, ITableEvents, IBridgeTableEvents)
 
 
     def __init__(self):
@@ -37,7 +55,7 @@ class EventHandler:
 
 
     def connectionLost(self, connector, reason):
-        print "lost connection -", reason.getErrorMessage()
+        print "Lost connection:", reason.getErrorMessage()
         self.runCallbacks('connectionLost', connector, reason)
 
 
