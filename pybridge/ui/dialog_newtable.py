9c01df324799d502cf1890eb49fd0d82c3f00875
umgangee <Unknown>
2007-04-06 20:07:59 +0000

    Fix call to client.joinTable(), improve "Could not create table" error message.

--- pybridge/ui/dialog_newtable.py
+++ pybridge/ui/dialog_newtable.py
@@ -19,6 +19,7 @@
 import gtk
 from wrapper import GladeWrapper
 
+from pybridge.network.client import client
 from manager import wm
 
 
@@ -38,9 +39,9 @@ class DialogNewtable(GladeWrapper):
     def createFailure(self, reason):
         error = reason.getErrorMessage()
         dialog = gtk.MessageDialog(parent=self.window, flags=gtk.DIALOG_MODAL,
-                                   type=gtk.MESSAGE_ERROR,
-                                   buttons=gtk.BUTTONS_OK)
-        dialog.set_markup(_('Could not create new table.'))
+                                type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_OK)
+        dialog.set_title(_('Could not create table'))
+        dialog.set_markup(_('The table could not be created.'))
         dialog.format_secondary_text(_('Reason: %s') % error)
 
         dialog.run()
@@ -56,11 +57,12 @@ class DialogNewtable(GladeWrapper):
 
     def on_okbutton_clicked(self, widget, *args):
         tableid = self.entry_tablename.get_text()
-        d = self.parent.joinTable(tableid, host=True)
+        d = client.joinTable(tableid, host=True)
         d.addCallbacks(self.createSuccess, self.createFailure)
 
 
     def on_tablename_changed(self, widget, *args):
+        # Disable the OK button if the table name field is empty.
         sensitive = self.entry_tablename.get_text() != ""
         self.okbutton.set_property('sensitive', sensitive)
 
