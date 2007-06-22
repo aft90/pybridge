b468556345a07b1a5da0e48efc9c571f64ef5cda
umgangee <Unknown>
2007-06-22 15:34:52 +0000

    Push version number to 0.3.0, add extra metadata.

--- pybridge/__init__.py
+++ pybridge/__init__.py
@@ -1,1 +1,10 @@
-__version__ = '0.2.1'
+"""
+PyBridge - a free online bridge game.
+"""
+
+__version__ = '0.3.0'
+
+__author__ = 'Michael Banks <michael@banksie.co.uk>'
+__license__ = 'GNU General Public License, Version 2 or later'
+__url__ = 'http://pybridge.sourceforge.net/'
+
