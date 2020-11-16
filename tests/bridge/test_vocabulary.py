3b256fb36457237bfc5043d20776d9616ddd68a9
Andrei Troie <andreitroie90@gmail.com>
2020-11-16 23:55:30 +0000

    Fix render_* tests for new colour style

--- tests/bridge/test_vocabulary.py
+++ tests/bridge/test_vocabulary.py
@@ -22,14 +22,14 @@ class TestVocabulary(unittest.TestCase):
         call = Mock(spec=Call.Bid)
         call.level = Level.Two
         call.strain = Strain.Diamond
-        with patch('pybridge.ui.vocabulary.config', { 'Appearance' : { 'Colours': {'Diamond': (0,0,0)}} }), patch('pybridge.ui.vocabulary.STRAIN_SYMBOLS', { Strain.Diamond: 'D' }):
+        with patch('pybridge.ui.vocabulary.config', { 'Appearance' : { 'Colours': {'Diamond': '000000000000'}} }), patch('pybridge.ui.vocabulary.STRAIN_SYMBOLS', { Strain.Diamond: 'D' }):
             self.assertEqual("2<span color='#000000000000'>D</span>", render_call(call))
 
     def test_render_call_suit_with_symbol(self):
         call = Mock(spec=Call.Bid)
         call.level = Level.Two
         call.strain = Strain.Diamond
-        with patch('pybridge.ui.vocabulary.config', { 'Appearance': { 'Colours': {'Diamond': (0,0,0) } } }), patch('pybridge.ui.vocabulary.STRAIN_SYMBOLS', { Strain.Diamond: '\N{BLACK DIAMOND SUIT}'}) :
+        with patch('pybridge.ui.vocabulary.config', { 'Appearance': { 'Colours': {'Diamond': '000000000000' } } }), patch('pybridge.ui.vocabulary.STRAIN_SYMBOLS', { Strain.Diamond: '\N{BLACK DIAMOND SUIT}'}) :
             self.assertEqual("2<span color='#000000000000'>\N{BLACK DIAMOND SUIT}</span>", render_call(call))
 
     def test_render_contract_undoubled(self):
@@ -41,7 +41,7 @@ class TestVocabulary(unittest.TestCase):
         call.strain = Strain.Spade
         contract.declarer = Direction.North
         contract.bid = call
-        with patch('pybridge.ui.vocabulary.config', { 'Appearance' : { 'Colours': {'Spade': (0,0,0)}} }), patch('pybridge.ui.vocabulary.STRAIN_SYMBOLS', { Strain.Spade: 'S' }):
+        with patch('pybridge.ui.vocabulary.config', { 'Appearance' : { 'Colours': {'Spade': '000000000000'}} }), patch('pybridge.ui.vocabulary.STRAIN_SYMBOLS', { Strain.Spade: 'S' }):
             self.assertEqual("4<span color='#000000000000'>S</span> by North", render_contract(contract))
 
     def test_render_contract_doubled(self):
@@ -64,7 +64,7 @@ class TestVocabulary(unittest.TestCase):
         call.strain = Strain.Heart
         contract.declarer = Direction.East
         contract.bid = call
-        with patch('pybridge.ui.vocabulary.config', { 'Appearance' : { 'Colours': {'Heart': (0,0,0)}} }), patch('pybridge.ui.vocabulary.STRAIN_SYMBOLS', { Strain.Heart: 'H' }):
+        with patch('pybridge.ui.vocabulary.config', { 'Appearance' : { 'Colours': {'Heart': '000000000000'}} }), patch('pybridge.ui.vocabulary.STRAIN_SYMBOLS', { Strain.Heart: 'H' }):
             self.assertEqual("3<span color='#000000000000'>H</span> XX by East", render_contract(contract))
 
     def test_redoubled_takes_precedence_over_doubled(self):
